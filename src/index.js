ee829aa5af8664bb7f1668db132c329e7edc2985
leealexeyvl <leealexey_94@mail.ru>
2021-03-13 00:08:37 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// module.exports = function reverse (n) {
+//     if (n.toString()[0] == '-') {
+//         return +n.toString().slice(1).split('').reverse().join('');
+//     } else {
+//         return +n.toString().split('').reverse().join('');
+//     }
+// }
 module.exports = function reverse (n) {
-    if (n.toString()[0] == '-') {
-        return +n.toString().slice(1).split('').reverse().join('');
-    } else {
-        return +n.toString().split('').reverse().join('');
-    }
+    return Math.abs(n).toString().split('').reverse().join('');
 }
